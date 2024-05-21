a1531d82931d7493b70a9c8c16d6f96f9a15163c
Abhijit Chakraborty <115333386+Abhijit-2002@users.noreply.github.com>
2024-05-21 21:46:38 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,32 +1,29 @@
-let slideBtnleft = document.getElementById("slide-btn-left")
-let slideBtnright = document.getElementById("slide-btn-right")
-let imgItem = document.querySelectorAll(".img-item")
+let slideBtnleft = document.getElementById("slide-btn-left");
+let slideBtnright = document.getElementById("slide-btn-right");
+let imgItem = document.querySelectorAll(".img-item");
 
-console.log(imgItem)
+console.log(imgItem);
 let startSlider = 0;
-let endSlider= (imgItem.length-1)*100
+let endSlider = (imgItem.length - 1) * 100;
 
-slideBtnleft.addEventListener("click",()=>{
-   // alert("left btn")
-    if (startSlider < 0){
-        startSlider = startSlider+100;
-    }
-    // startSlider = startSlider -100;
-    imgItem.forEach(element =>{
-        element.style.transform = ` translateX(${startSlider}%)`
-    })
-})
-slideBtnright.addEventListener("click",()=>{
-    //alert("right btn")
+slideBtnleft.addEventListener("click", () => {
+  // alert("left btn")
+  if (startSlider < 0) {
+    startSlider = startSlider + 100;
+  }
+  // startSlider = startSlider -100;
+  imgItem.forEach((element) => {
+    element.style.transform = ` translateX(${startSlider}%)`;
+  });
+});
+slideBtnright.addEventListener("click", () => {
+  //alert("right btn")
 
-    if (startSlider >= -endSlider+100){
-        startSlider = startSlider-100;
-    }
-    // startSlider = startSlider -100;
-    imgItem.forEach(element =>{
-        element.style.transform = ` translateX(${startSlider}%)`
-    })
-    
-    
-    
-})
+  if (startSlider >= -endSlider + 100) {
+    startSlider = startSlider - 100;
+  }
+  // startSlider = startSlider -100;
+  imgItem.forEach((element) => {
+    element.style.transform = ` translateX(${startSlider}%)`;
+  });
+});
